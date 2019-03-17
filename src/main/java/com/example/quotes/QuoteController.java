53183d555ff194046f33972fe6dfbc881f520386
adib <AlexKonrad$1827%*>
2019-03-16 21:37:41 -0400

    Add Spring Security via Form Based Login

--- src/main/java/com/example/quotes/QuoteController.java
+++ src/main/java/com/example/quotes/QuoteController.java
@@ -1,13 +1,7 @@
 package com.example.quotes;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class QuoteController {
@@ -22,21 +16,5 @@ public class QuoteController {
     public Quote radomQuote()
     {
         return quoteRepository.findRandomQuote();
-    }
-
-    @GetMapping("/quotes")
-    public List<Quote> getAll()
-    {
-        return quoteRepository.findAll();
-    }
-
-    @GetMapping("/quotes/{id}")
-    public ResponseEntity<Quote> getQuote(@PathVariable("id") Integer id) {
-        Optional<Quote> quote = quoteRepository.findById(id);
-        if (quote.isPresent()) {
-            return new ResponseEntity<>(quote.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
     }
 }
